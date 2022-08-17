4757c5e5fd18476003e2abdb602e4190d69045c6
tymowskyy <tymowskyy@gmail.com>
2022-08-17 19:18:34 +0200

    used '\r' to update current bar in terminal

--- progress.py
+++ progress.py
@@ -13,10 +13,10 @@ class ProgressBar:
     def display(self, progress):
         n_full_chars = round(progress * self.__size)
         n_empty_chars = self.__size - n_full_chars
-        print('[' + self.__full_char * n_full_chars + self.__empty_char * n_empty_chars + ']')
+        print('[' + self.__full_char * n_full_chars + self.__empty_char * n_empty_chars + ']', end='\r')
 
     def __enter__(self):
-        print(self.__HIDE_CURSOR)
+        print(self.__HIDE_CURSOR, end='')
         return self
     
     def __exit__(self, *exc):
@@ -26,4 +26,8 @@ if __name__ == '__main__':
     with ProgressBar() as progress_bar:
         for i in range(100):
             progress_bar.display(i/99)
-            time.sleep(.1)
+            time.sleep(.05)
+    with ProgressBar(size=15, full_char='+', empty_char='-') as progress_bar:
+        for i in range(100):
+            progress_bar.display(i/99)
+            time.sleep(.02)
