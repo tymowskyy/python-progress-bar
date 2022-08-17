c94e5592e4b58fabd11730ce063faa911be497e7
tymowskyy <tymowskyy@gmail.com>
2022-08-17 19:10:45 +0200

    ProgressBar is now a context manager you can use ProgressBar using with statement

--- progress.py
+++ progress.py
@@ -2,6 +2,8 @@ import time
 
 
 class ProgressBar:
+    __HIDE_CURSOR = "\x1b[?25l"
+    __SHOW_CURSOR = "\x1b[?25h"
 
     def __init__(self, size=10, full_char='#', empty_char=' '):
         self.__size = size
@@ -13,8 +15,15 @@ class ProgressBar:
         n_empty_chars = self.__size - n_full_chars
         print('[' + self.__full_char * n_full_chars + self.__empty_char * n_empty_chars + ']')
 
+    def __enter__(self):
+        print(self.__HIDE_CURSOR)
+        return self
+    
+    def __exit__(self, *exc):
+        print(self.__SHOW_CURSOR)
+
 if __name__ == '__main__':
-    progress_bar = ProgressBar()
-    for i in range(100):
-       progress_bar.display(i/99)
-       time.sleep(.1)
+    with ProgressBar() as progress_bar:
+        for i in range(100):
+            progress_bar.display(i/99)
+            time.sleep(.1)
